72845aa9c2141a27cf9e0c92b63af6667aaf7a62
Arthur Romanha <126591597+ArthurRomanha@users.noreply.github.com>
2025-03-24 11:05:28 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-let btnRelativeAtTheProject = document.getElementById(`conto${idx+1}`);
+// let btnRelativeAtTheProject = document.getElementById(`conto${idx+1}`);
 
-const carousel = () => {
-    clearInterval(interval);
-    interval = setInterval(carousel, 2000);
+// const carousel = () => {
+//     clearInterval(interval);
+//     interval = setInterval(carousel, 2000);
 
-    idx++;
+//     idx++;
 
-    if (idx > project.length - 1) {
-        idx = 0;
-    }
-    btnRelativeAtTheProject = document.getElementById(`project${idx + 1}`);
-    projects.style.transform = `translateX(${-idx * 277}px)`;
-    btnRelativeAtTheProject.classList.add("projectSelected");
-    btnRelativeAtTheProject.classList.remove("projectsUnselected");
-}
+//     if (idx > project.length - 1) {
+//         idx = 0;
+//     }
+//     btnRelativeAtTheProject = document.getElementById(`project${idx + 1}`);
+//     projects.style.transform = `translateX(${-idx * 277}px)`;
+//     btnRelativeAtTheProject.classList.add("projectSelected");
+//     btnRelativeAtTheProject.classList.remove("projectsUnselected");
+// }
 
-let interval = setInterval(carousel, 2000);
+// let interval = setInterval(carousel, 2000);
